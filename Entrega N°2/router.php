bda88ae25eab8e9430dacbcc39312321265c4013
TobiasMastroberardini <132698757+TobiasMastroberardini@users.noreply.github.com>
2023-10-18 01:23:18 -0300

    Update router.php

--- Entrega N°2/router.php
+++ Entrega N°2/router.php
@@ -2,6 +2,7 @@
 require_once './.controller/auth.controller.php';
 require_once './.controller/page.controller.php';
 require_once './.controller/subscripciones.controller.php';
+require_once './.controller/admin.controller.php';
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' .$_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
 $action = 'miPerfil';
@@ -43,6 +44,40 @@ switch($params[0]){
     case 'subscripciones':
         $controller = new subscripcionesController();
         $controller->ShowSubs();
+        break;
+
+    //
+    case 'administracion':
+        $controller = new adminController();
+        $controller->mostrarAdmin();
+        break;
+    case 'eliminarCategoria':
+        $controller = new adminController();
+        $controller->eliminarCategoria($params[1]);
+        break;
+    case 'editarCategoria':
+        $controller = new adminController();
+        $controller->editarCategoria($params[1]);
+        break;
+    case 'agregarCategoria':
+        $controller = new adminController();
+        $controller->agregarCategoria();
+        break;
+    case 'agregarItem':
+        $controller = new adminController();
+        $controller->agregarItem();
+        break;
+    case 'eliminarItem':
+        $controller = new adminController();
+        $controller->eliminarItem($params[1]);
+        break;
+    case 'mostrarEditarItem':
+        $controller = new adminController();
+        $controller->mostrarFormEditar($params[1]);
+        break;
+    case 'editarItem':
+        $controller = new adminController();
+        $controller->editarItem($params[1]);
         break;
     default;
         echo 'error404';
